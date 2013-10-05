cd8b3c5880d98d9fdd60c0dc5c2704156bc16982
abalone <gorgeousabalone@gmail.com>
2013-10-05 12:32:37 +0800

    solve clone-graph and merge-intervals

--- Solutions/clone-graph.cpp
+++ Solutions/clone-graph.cpp
@@ -11,32 +11,30 @@ public:
     UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
         
-		vector<UndirectedGraphNode*> copiedNodes(100, nullptr);
-
-		return cloneGraph(node, copiedNodes);
-    }
-
-private:
-	UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node, 
-			vector<UndirectedGraphNode*> &copiedNodes) {
-
 		if (node == NULL)
 			return NULL;
 
-		if (copiedNodes.size() <= node->label)
-			copiedNodes.resize((node->label+1)*2, nullptr);
+		queue<UndirectedGraphNode*> nodeQueue;
+		unordered_map<int, UndirectedGraphNode*> copiedNodes;
+		copiedNodes[node->label] = new UndirectedGraphNode(node->label);
+		nodeQueue.push(node);
 
-		UndirectedGraphNode *cur = new UndirectedGraphNode(node->label);
-		copiedNodes[cur->label] = cur;
-		for (int i=0; i<node->neighbors.size(); i++) {
-			int curLabel = node->neighbors[i]->label;
-			if (curLabel<copiedNodes.size() && copiedNodes[curLabel]) {
-				cur->neighbors.push_back(copiedNodes[curLabel]);
-			} else {
-				cur->neighbors.push_back(cloneGraph(node->neighbors[i]));
+		while (!nodeQueue.empty()) {
+			UndirectedGraphNode *top = nodeQueue.front();
+			UndirectedGraphNode *copied = copiedNodes[top->label];
+			nodeQueue.pop();
+
+			for (int i=0; i<top->neighbors.size(); i++) {
+				UndirectedGraphNode *cur = top->neighbors[i];
+				if (copiedNodes.count(cur->label) == 0) {
+					nodeQueue.push(cur);
+					copiedNodes[cur->label] = new UndirectedGraphNode(cur->label);
+				}
+
+				copied->neighbors.push_back(copiedNodes[cur->label]);
 			}
 		}
 
-		return cur;
-	}
+		return copiedNodes[node->label];
+    }
 };
